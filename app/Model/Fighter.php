6141ef1799356be18c385d2eda43d2e0c4b824d2
Matthieu <matthieu.boucon@outlook.com>
2014-11-10 17:57:53 +0100

    prout

--- app/Model/Fighter.php
+++ app/Model/Fighter.php
@@ -29,5 +29,15 @@ class Fighter extends AppModel {
         ),
 
    );
+    
+   public function doMove($fighterId, $direction){
+       if($direction='nord') 
+               $coor=$this->fighters->read(null,1);
+               
+               echo $coor;
+               $this->fighters->set('coordinate_x',$coor+1);
+               $this->fighters->save();
+       
+   }
 
 }
