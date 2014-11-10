a44b935f909be4cc6d21ba4fab0bf4d2030ae811
Matthieu <matthieu.boucon@outlook.com>
2014-11-10 18:53:52 +0100

    doAttack

--- app/Model/Fighter.php
+++ app/Model/Fighter.php
@@ -30,14 +30,28 @@ class Fighter extends AppModel {
 
    );
     
-   public function doMove($fighterId, $direction){
-       if($direction='nord') 
-               $coor=$this->fighters->read(null,1);
-               
-               echo $coor;
-               $this->fighters->set('coordinate_x',$coor+1);
-               $this->fighters->save();
-       
-   }
+function doMove($fighterId, $direction)
+    {
+        // récupérer la position et fixer l'id de travail
+        $datas = $this->read(null, $fighterId);
+
+        //@todo empêcher le joueur de sortir des limites du terrain
+        //@todo empêcher le joueur de se déplacer sur une case occupée
+        // falre la modif
+        if ($direction == 'north')
+            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] + 1);
+        elseif ($direction == 'south')
+            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] - 1);
+        elseif ($direction == 'east')
+            $this->set('coordinate_y', $datas['Fighter']['coordinate_y'] + 1);
+        elseif ($direction == 'west')
+            $this->set('coordinate_y', $datas['Fighter']['coordinate_y'] - 1);
+        else
+            return false;
+
+        // sauver la modif
+        $this->save();
+        return true;
+    }
 
 }
