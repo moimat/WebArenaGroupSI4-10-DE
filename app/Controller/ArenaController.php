2772d4ef53bc8d56c44784ffff249d6f95fea62d
NicolasGriere <griere@ece.fr>
2014-12-01 10:21:28 +0100

    Attack dans le vide gérée

--- app/Controller/ArenaController.php
+++ app/Controller/ArenaController.php
@@ -135,12 +135,12 @@ class ArenaController extends AppController {
             }
             if (isset($this->request->data['Fighteratk'])) {
                 $eventArray = $this->Fighter->doAttack($currentFighterId, $this->request->data['Fighteratk']['direction']);
-                if ($eventArray["coordinate_x"] != NULL && $eventArray["coordinate_y"] != NULL) {
+                if ($eventArray["coordinate_x"] != NULL && $eventArray["coordinate_y"] != NULL && $eventArray["name"] != NULL) {
                     $this->Event->createEvent($eventArray["coordinate_x"], $eventArray["coordinate_y"], $eventArray["date"], $eventArray["name"]);
                     $this->Session->setFlash('Une attaque a été réalisée.');
                     
                     $eventArrayDead = $this->Fighter->eliminateDead();
-                    if ($eventArrayDead["coordinate_x"] != NULL && $eventArrayDead["coordinate_y"] != NULL) {
+                    if ($eventArrayDead["coordinate_x"] != NULL && $eventArrayDead["coordinate_y"] != NULL && $eventArrayDead["name"] != NULL) {
                         $this->Event->createEvent($eventArrayDead["coordinate_x"], $eventArrayDead["coordinate_y"], $eventArrayDead["date"], $eventArrayDead["name"]);
                         $this->Session->setFlash('Une comattant est mort.');
                     }
